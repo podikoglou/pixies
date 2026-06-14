484b21d978ad06a2721df974e8110d4c3cc6a4eb
alex <alex.podikoglou@gmail.com>
2026-06-14 23:51:07 +0300

    chore: remove migrated src/, point root scripts at workspace packages

--- src/tools/index.ts
+++
@@ -1,8 +0,0 @@
-import type { AgentTool } from "@earendil-works/pi-agent-core";
-import { geocodeTool } from "./geocode.ts";
-import { queryOsmTool } from "./query-osm.ts";
-import { reverseGeocodeTool } from "./reverse-geocode.ts";
-
-export const tools: AgentTool[] = [geocodeTool, reverseGeocodeTool, queryOsmTool];
-
-export { geocodeTool, queryOsmTool, reverseGeocodeTool };
