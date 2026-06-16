5d65ff3b7e583004df8b4253b5bf1d2690605d3e
alex <alex.podikoglou@gmail.com>
2026-06-16 20:49:14 +0300

    feat(core): add createLogger factory and ./logging subpath export

---
+++ packages/core/src/logging/index.ts
@@ -0,0 +1,2 @@
+export { createLogger, silentLogger, type CreateLoggerOptions, type Logger } from "./logger.ts";
+export { DiscordTransport, type DiscordTransportOptions } from "./discord-transport.ts";
